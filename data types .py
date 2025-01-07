df156731ab5658b6b22a03ff05306f0bbd394d69
kevin <bridget.nmaclou@gmail.com>
2025-01-07 12:47:14 +0200

    first commit

---
+++ data types .py
@@ -0,0 +1,10 @@
+name = input("enter the name:")
+print(name,type(name))
+
+age = int(input("enter the age:"))
+print(age,type(age) )
+
+weight = float(input("enter the weight:"))
+print(weight,type(weight))
+
+
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+print(isstudent,type(isstudent))
